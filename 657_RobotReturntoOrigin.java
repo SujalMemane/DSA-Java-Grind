1973509955ac81b2edbc4d72e53e4cd92e708018
SujalMemane <sujal.memane23@gmail.com>
2025-09-13 22:32:35 +0530

    sloved 657. Robot Return to Origin

--- 657_RobotReturntoOrigin.java
+++ 657_RobotReturntoOrigin.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean judgeCircle(String moves) {
         int x = 0 , y = 0 ;
-        for(int i = 0 ; i < moves.length() ; i++){
-            char c = moves.charAt(i);
+        for(char c : moves.toCharArray()){
             if( c == 'U') y++;
-            if( c == 'D') y--;
-            if( c == 'L') x--;
-            if( c == 'R') x++;
+            else if( c == 'D') y--;
+            else if( c == 'L') x--;
+            else if( c == 'R') x++;
         }
         return x == 0 && y == 0;
     }
