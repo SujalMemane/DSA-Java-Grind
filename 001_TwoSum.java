93354dd292ff5c2d5d4c86376db9179b474ef122
SujalMemane <sujal.memane23@gmail.com>
2025-09-21 11:23:05 +0530

    improve 1. Two Sum

--- 001_TwoSum.java
+++ 001_TwoSum.java
@@ -10,3 +10,21 @@ class Solution {
         return new int[] {};
     }
 }
+
+// better way 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for( int i = 0 ; i < nums.length ; i++){
+            int req = target - nums[i];
+
+            if(map.containsKey(req)){
+                return new int[]{map.get(req) , i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
