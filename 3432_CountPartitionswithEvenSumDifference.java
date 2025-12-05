f189fd6f94b8b56e5bed69cf17138e5fe917e613
SujalMemane <sujal.memane23@gmail.com>
2025-12-05 22:32:31 +0530

    Sloved Q 3432. Count Partitions with Even Sum Difference  -  with better approach

--- 3432_CountPartitionswithEvenSumDifference.java
+++ 3432_CountPartitionswithEvenSumDifference.java
@@ -21,3 +21,28 @@ class Solution {
         return partitions ;
     }
 }
+
+/*Let
+leftSum = x
+rightSum = y
+Total sum = S = x + y
+You need (x − y) to be even.
+✔ When is (x − y) even?
+
+Because:
+x − y = x − (S − x) = 2x − S
+2x is always even → so (2x − S) is even iff S is even.
+
+👉 So the entire answer depends only on the total sum S:
+If S is even, then all partitions (n − 1) are valid.
+If S is odd, then 0 partitions are valid. */
+
+class Solution {
+    public int countPartitions(int[] nums) {
+        int total = 0 ;
+
+        for( int i : nums) total += i ;
+
+        return (total % 2 == 0) ? nums.length - 1 : 0 ;
+    }
+}
