d49efe7976603059e2916865e659b6c141d326b5
SujalMemane <sujal.memane23@gmail.com>
2025-12-10 18:27:25 +0530

    Sloved 463. Island Perimeter

---
+++ 463_IslandPerimeter.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        
+        int rows = grid.length;
+        int colm = grid[0].length;
+        int perimeter = 0 ;
+
+        for(int i = 0 ; i < rows ; i++){
+            for( int j = 0 ; j < colm ; j++){
+
+                if(grid[i][j] == 1){
+                    perimeter += 4;
+
+                    if( i > 0 && grid[i - 1][j] == 1 ) perimeter -= 1;
+
+                    if( i < rows - 1 && grid[i + 1][j] == 1 ) perimeter -= 1;
+
+                    if( j > 0 && grid[i][j - 1] ==  1) perimeter -= 1;
+
+                    if( j < colm - 1  && grid[i][j + 1] == 1 ) perimeter -=1 ;
+                }
+            }
+        }
+        return perimeter ;
+    }
+}
